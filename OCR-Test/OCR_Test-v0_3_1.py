7648739d5680dcd390d757090c91c57adced953e
jmichaelmenina <menina.jmichael@gmail.com>
2023-01-08 22:53:10 +0800

    OCR-only fixes

--- OCR-Test/OCR_Test-v0_3_1.py
+++ OCR-Test/OCR_Test-v0_3_1.py
@@ -57,12 +57,7 @@ def resizeImage(img:Image.Image) -> Image.Image:
     resized_img = img.resize(new_size, Image.LANCZOS)
     
     return resized_img
-    
-    temp_filename = (tempfile.NamedTemporaryFile(delete=True, suffix='_TEMP.png')).name
-    resized_img.save(temp_filename, dpi=(300, 300))
-    
-    return Image.open(temp_filename)
-    
+
 def denoiseImage(img:Image.Image) -> Image.Image:
     """ Removes noise in an image. Returns an Image type. """
     
@@ -97,8 +92,8 @@ def getImageContours(img:cv2.Mat) -> tuple:
     return contours
 
 def fixNumTypos(string:str) -> str:
-    """ Fixes known numerical typos in string """
-    correction_dict = {"0": "O", "1":"IL", "5":"?"}
+    """ Fixes known numerical typos in string. """
+    correction_dict = {"0": "O", "1":"IL", "5":"§"}
     string = string.upper()
     
     for correction in correction_dict:
@@ -111,7 +106,7 @@ def fixNumTypos(string:str) -> str:
 ############################################
 
 def main() -> None:
-    version = "0.3"
+    version = "0.3.1"
     print(f"Version {version}\n")
     
     # Set-up input image
@@ -146,9 +141,9 @@ def main() -> None:
     cv2.imwrite(preprocessed_filename, np.array(preprocessed_img))
 
     # Threshold
-    preprocessed_img = thresholdImage(preprocessed_img)
-    printImageAttributes(preprocessed_img, "THRESHOLD")
-    cv2.imwrite(preprocessed_filename, np.array(preprocessed_img))
+    # preprocessed_img = thresholdImage(preprocessed_img)
+    # printImageAttributes(preprocessed_img, "THRESHOLD")
+    # cv2.imwrite(preprocessed_filename, np.array(preprocessed_img))
 
     # Get histogram and get page-wide lines containing text
     inverted_img = invertImage(preprocessed_img)
@@ -166,94 +161,126 @@ def main() -> None:
         cv2.line(text_lines_img_RGB_mat, (0, l), (w, l), (0, 255, 0), 1)
     cv2.imwrite(text_lines_filename, text_lines_img_RGB_mat)
 
-    # Use Tesseract OCR to read text per line
+    # Prepare to use Tesseract OCR
     preprocessed_img_mat = np.array(preprocessed_img)
     
     ocr_config = "--psm 7"
     # ocr_config = "--psm 7 -c tessedit_char_whitelist=abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ(\'\"\"\').,:;?!%&123456789+-*/="
     
-    line_vertical_boundary_offset = 3
-    lines_stack = []
+    line_vertical_offset = 3
+    line_padding = np.ones((10, w), dtype = np.uint8)*255
     
+    recognized_text = ""
+    word_delimiter = "<!word_end!>"
+    
+    # OPTIONAL: See how each lines were processed in an output image
+    lines_stack = []
     divider_RGB = np.zeros((50, w, 3), dtype = np.uint8)
-    padding_L = np.ones((10, w), dtype = np.uint8)*255
     stack_header = np.zeros((50, w, 3), dtype = np.uint8)
     cv2.putText(
         stack_header, f"VERSION {version}", 
         (5, stack_header.shape[0]//2 + 2), cv2.FONT_HERSHEY_SIMPLEX, 
         0.5, (255, 255, 255), 2)
     lines_stack.append(stack_header)
-
-    recognized_text = ""
-    word_delimiter = "<!word_end!>"
+    
+    # For each line, recognize text
     print("########## LOG ##########")
     for u, l in zip(upper_line_bounds, lower_line_bounds):        
         # Crop the portion of the image containing the line, then pad the top and bottom with white spaces
-        line_img = preprocessed_img_mat[u - line_vertical_boundary_offset : l + line_vertical_boundary_offset,]
-        line_img = np.concatenate((padding_L, line_img, padding_L), dtype=np.uint8)
+        line_img = preprocessed_img_mat[u - line_vertical_offset : l + line_vertical_offset,]
+        line_img = np.concatenate((line_padding, line_img, line_padding), dtype=np.uint8)
         
         # Create contours to detect potential text
         contours = getImageContours(line_img)[::-1]
         
+        # For each section of the line possibly containing text, recognize text
         line_data = (cv2.cvtColor(line_img,cv2.COLOR_GRAY2RGB)).copy()
         for cont in contours:
             x_cont, y_cont, w_cont, h_cont = cv2.boundingRect(cont)
 
             # Crop the text block and recognize text in the block
             line_section = line_img[y_cont : y_cont + h_cont, x_cont : x_cont + w_cont]            
-            line_text = pytesseract.image_to_string(line_section, config=f"{ocr_config}")
             
-            # Draw a rectangle marking the section and the recognized text (marked "?" if not ASCII) from the section
+            # Preprocess section by inrange thresholding for better accuracy
+            _hsv = cv2.cvtColor(cv2.cvtColor(line_section, cv2.COLOR_GRAY2BGR), cv2.COLOR_BGR2HSV)
+            _mask = cv2.inRange(_hsv, np.array([0, 0, 0]), np.array([179, 255, 80]))
+            _kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 3))
+            _dilated = cv2.dilate(_mask, _kernel, iterations=1)
+            line_section = 255 - cv2.bitwise_and(_dilated, _mask)
+            
+            line_text = pytesseract.image_to_string(line_section, config=f"{ocr_config}")
+            line_text = "".join([c if ord(c) < 128 else c for c in line_text]).strip()
+            
+            # NOTE: All read text will be stored in uppercase
+            recognized_text += line_text.upper() + word_delimiter
+            
+            # OPTIONAL: Draw a rectangle marking the section and the recognized text (marked "?" if not ASCII) from the section
             cv2.rectangle(
                 line_data, 
                 (x_cont, y_cont), (x_cont + w_cont, y_cont + h_cont), 
                 (0, 255, 0), 2)
-            line_text = "".join([c if ord(c) < 128 else "?" for c in line_text]).strip()
             cv2.putText(
                 line_data, line_text, 
                 (x_cont + 3, y_cont+10), cv2.FONT_HERSHEY_SIMPLEX, 
                 0.35, (200, 0, 0), 1)
-            
-            recognized_text += line_text.upper() + word_delimiter
             print(line_text)
+            cv2.imwrite(results_filename, line_section)
+            
 
         recognized_text += "\n"
+        
+        # OPTIONAL: See how each lines were processed in an output image
         lines_stack.append(line_data)
         lines_stack.append(divider_RGB)
+    print("#########################")
 
+    # OPTIONAL: See how each lines were processed in an output image
     cropped_lines_img = np.vstack(lines_stack)
     cv2.imwrite(results_filename, cropped_lines_img)
     
+    # Finally, save all recognized text to a text file then process all recognized text; store output in a csv
     with open("recognized_text.txt", "w") as out_file:
         out_file.write(recognized_text.replace(word_delimiter, " "))
-    print("#########################")
-    
+
     with open(csv_filename, "w") as csv_file:
+        # csv_file.write("LINE,BEARING,DISTANCE,FORMULA\n")
+        csv_file.write("LINE,N/S,DEG,MIN,E/W,DISTANCE,FORMULA\n")
+        
         recognized_text = [i.split(word_delimiter) for i in recognized_text.split("\n")]
+        
         td_keywords = {"LINE":-1, "BEARING":-1, "DISTANCE":-1}
         num_of_cols = -1
         
-        csv_file.write("LINE,BEARING,DISTANCE,FORMULA\n")
+        # Find the line containing the TD keywords; process succeeding lines until an irrelevant line is encountered
         for line in recognized_text:
             if all(np.array([*td_keywords.values()]) != -1) and len(line) == num_of_cols:
-                _bearing_raw = fixNumTypos(line[td_keywords['BEARING']])
-                _bearing_split = [re.search('^[NS\$]', _bearing_raw), re.search('\d+[^-]*-', _bearing_raw), re.search('-[^-]*\d+', _bearing_raw), re.search('[WE]$', _bearing_raw)]
+                # The bearing field can vary; for proper handling, split up the string
+                _bearing = fixNumTypos(line[td_keywords['BEARING']])
+                _bearing_split = [
+                    re.search('^[NS\$]', _bearing), 
+                    re.search('\d+[^-]*-', _bearing), 
+                    re.search('-[^-]*\d+', _bearing), 
+                    re.search('[WE]$', _bearing)
+                    ]
+                
+                # Bearing values have the most complex formatting; this will be the basis on whether or not the table has ended
                 if not all(_bearing_split):
                     break
-                    
+                
+                # Process bearing components
                 ns_f, deg_f, min_f, ew_f = [*map(lambda m: m.group(), _bearing_split)]
                 ns_f = "S" if "$" in ns_f else ns_f
                 deg_f = re.search('\d+', deg_f).group()
                 min_f = re.search('\d+', min_f).group()
                 
+                # Finalize line to be written in the csv
                 _line = fixNumTypos(line[td_keywords['LINE']])
                 _distance = fixNumTypos(line[td_keywords['DISTANCE']])
                 _distance = re.search('^([0-9]|\.|,)+', _distance).group()
-                _bearing = f"{ns_f}{deg_f}-{min_f}{ew_f}"
                 
                 _formula = f"@{_distance}<{ns_f}{deg_f}d{min_f}'{ew_f}"
                 
-                csv_file.write(f"{_line},{_bearing},{_distance},{_formula}\n")
+                csv_file.write(f"{_line},{ns_f},{deg_f},{min_f},{ew_f},{_distance},{_formula}\n")
     
             elif all([i in line for i in td_keywords]):
                 td_keywords.update(zip(td_keywords, [line.index(i) for i in td_keywords]))
